e651727403cd66220a5110d232c8f815d3c2df9f
James Mullinix <james.mullinix@bossanova.com>
2018-12-19 10:00:54 -0800

    tweak histogram plotter to use BCa CI

--- plot_hist.R
+++ plot_hist.R
@@ -25,19 +25,19 @@ plot_histogram=function(filename){
   colnames(CIdata)=c('lower_percentile','upper_percentile','lower_BCa','upper_BCa','lower_SEM', 'upper_SEM','middle','mean','npts','Nbs')
   
   # prepare legend
-  legendlabels=c("95% CI (Basic)","SEM","Median","Mean")
+  legendlabels=c("95% CI (BCa)","SEM","Median","Mean")
   names(tableau_colors)=legendlabels
   
   # prepare plots of CIs and centrality
   linesize=0.5
-  vlines=data.frame(values=c(CIdata$lower_percentile,CIdata$lower_SEM,CIdata$middle,CIdata$mean),Legend=legendlabels)
+  vlines=data.frame(values=c(CIdata$lower_BCa,CIdata$lower_SEM,CIdata$middle,CIdata$mean),Legend=legendlabels)
   
   # create ggplot object
   p=ggplot(histogramdata, aes(x=slope, y=frequency)) + 
     geom_bar(stat="identity",width=bin_size) +
     geom_vline(data=vlines, aes(xintercept=values, color=Legend), show.legend=TRUE, size=linesize)+
     geom_vline(xintercept = CIdata$upper_SEM,show.legend=FALSE,color=tableau_colors[2], size=linesize) + 
-    geom_vline(xintercept = CIdata$upper_percentile,show.legend=FALSE,color=tableau_colors[1], size=linesize) +
+    geom_vline(xintercept = CIdata$upper_BCa,show.legend=FALSE,color=tableau_colors[1], size=linesize) +
     scale_color_manual("",values = tableau_colors)+
     theme_classic(base_family="Helvetica",base_size=9)+
     theme(text=element_text(size=9))
